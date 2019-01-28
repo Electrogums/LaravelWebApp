c4cd5d231dcc9aef2661c42c05a4fd71dfe3d1d7
EFLLP <erickllanos30@gmail.com>
2019-01-27 20:45:17 -0600

    Agregue Migraciones y facotires.
    *.- Agregue todas las migraciones ademas de empezar a utilizar Eloquen.

---
+++ 01 primerapp/holamundo/app/level.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * App\level
+ *
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\level newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\level newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\level query()
+ * @mixin \Eloquent
+ * @property int $id
+ * @property string $name
+ * @property string $description
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\level whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\level whereDescription($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\level whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\level whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\level whereUpdatedAt($value)
+ */
+class level extends Model
+{
+    public function courses () {
+    	return $this->hasMany(Course::class);
+    }
+}
